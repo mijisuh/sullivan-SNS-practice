b3f82cc4f87aa7575ada6e886c7cb5524bf74a83
mijisuh <itsanisland@naver.com>
2021-08-21 15:32:50 +0900

    환경변수 등록

---
+++ firestores/firebaseConfig.js
@@ -0,0 +1,11 @@
+// 채우기
+export const firebaseConfig = {
+    apiKey: process.env.ARI_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID
+  };
+ 
+  export default firebaseConfig;
